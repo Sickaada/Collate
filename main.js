649020d919b38f9185920e63bc889e2aca1edaf7
Sweety Gupta <sweety_g@cs.iitr.ac.in>
2021-01-10 02:56:32 +0530

    changedfront

--- main.js
+++ main.js
@@ -16,23 +16,33 @@ function pythonClick(){
     document.getElementById("cppButton").style.backgroundColor = "#ffffff";
     document.getElementById("pythonButton").style.backgroundColor = "#ff6700" ; 
     document.getElementById("javaButton").style.backgroundColor = "#ffffff";
-
+    
+    document.getElementById("cppButton").style.color = "#575757";
+    document.getElementById("pythonButton").style.color = "#ffffff" ; 
+    document.getElementById("javaButton").style.color = "#575757";
     
 }
+
 function cppClick(){
     document.getElementById("cppButton").style.backgroundColor = "#ff6700";
     document.getElementById("pythonButton").style.backgroundColor = "#ffffff";
     document.getElementById("javaButton").style.backgroundColor = "#ffffff";
 
-  
+    document.getElementById("cppButton").style.color = "#ffffff";
+    document.getElementById("pythonButton").style.color = "#575757" ; 
+    document.getElementById("javaButton").style.color = "#575757";
 }
+
 function javaClick(){
     document.getElementById("cppButton").style.backgroundColor = "#ffffff";
     document.getElementById("pythonButton").style.backgroundColor = "#ffffff";
     document.getElementById("javaButton").style.backgroundColor = "#ff6700";
     
-   
+    document.getElementById("cppButton").style.color = "#575757";
+    document.getElementById("pythonButton").style.color = "#575757" ; 
+    document.getElementById("javaButton").style.color = "#ffffff";
 }
+
 resetCodeBtn.addEventListener('click', () => {
     // Clear ace editor
     codeEditor.setValue(defaultCode);
