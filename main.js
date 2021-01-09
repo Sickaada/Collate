be9c160c3a88491e51e14341ca865390b6869528
Sweety Gupta <sweety_g@cs.iitr.ac.in>
2021-01-10 00:00:53 +0530

    changed frontend

--- main.js
+++ main.js
@@ -1,112 +1,37 @@
-// Retrieve Elements
-const consoleLogList = document.querySelector('.editor__console-logs');
-const executeCodeBtn = document.querySelector('.editor__run');
-const resetCodeBtn = document.querySelector('.editor__reset');
-
-// Setup Ace
-let codeEditor = ace.edit("editorCode");
-let defaultCode = '';
-let consoleMessages = [];
-
-let editorLib = {
-    clearConsoleScreen() {
-        consoleMessages.length = 0;
-
-        // Remove all elements in the log list
-        while (consoleLogList.firstChild) {
-            consoleLogList.removeChild(consoleLogList.firstChild);
-        }
-    },
-    printToConsole() {
-        consoleMessages.forEach(log => {
-            const newLogItem = document.createElement('li');
-            const newLogText = document.createElement('pre');
-
-            newLogText.className = log.class;
-            newLogText.textContent = `> ${log.message}`;
-
-            newLogItem.appendChild(newLogText);
-
-            consoleLogList.appendChild(newLogItem);
-        })
-    },
-    init() {
-        // Configure Ace
-
-        // Theme
-        codeEditor.setTheme("ace/theme/dreamweaver");
-
-        // Set language
-            codeEditor.session.setMode("ace/mode/javascript");
 
 
-        // Set Options
-        codeEditor.setOptions({
-            fontFamily: 'Inconsolata',
-            fontSize: '12pt',
-            enableBasicAutocompletion: true,
-            enableLiveAutocompletion: true,
-        });
+function acceptinput(){
+   var input= document.getElementById("inputtext").text;
 
-        // Set Default Code
-        codeEditor.setValue(defaultCode);
-    }
 }
 
-// Events
-executeCodeBtn.addEventListener('click', () => {
-    // Clear console messages
-    editorLib.clearConsoleScreen();
-    
-    // Get input from the code editor
-    const userCode = codeEditor.getValue();
+function resetinput(){
 
-    // Run the user code
-    try {
-        new Function(userCode)();
-    } catch (err) {
-        console.error(err);
-    }
-
-    // Print to the console
-    editorLib.printToConsole();
-});
-
-function javascriptClick(){
-    document.getElementById("javascriptButton").style.backgroundColor = "#d3d3d3";
-    document.getElementById("htmlButton").style.backgroundColor = "#b5e7a0";
-    document.getElementById("pythonButton").style.backgroundColor = "#b5e7a0";
-    document.getElementById("cppButton").style.backgroundColor = "#b5e7a0";
-    document.getElementById("javaButton").style.backgroundColor = "#b5e7a0";
+    document.getElementById("inputtext").value = "" ;
 }
-function htmlClick(){
-    document.getElementById("javascriptButton").style.backgroundColor = "#b5e7a0"; 
-    document.getElementById("htmlButton").style.backgroundColor = "#d3d3d3";
-    document.getElementById("pythonButton").style.backgroundColor = "#b5e7a0";
-    document.getElementById("cppButton").style.backgroundColor = "#b5e7a0";
-    document.getElementById("javaButton").style.backgroundColor = "#b5e7a0";
-    
-}
+
+
+
 function pythonClick(){
-    document.getElementById("javascriptButton").style.backgroundColor = "#b5e7a0";
-    document.getElementById("htmlButton").style.backgroundColor = "#b5e7a0";
-    document.getElementById("pythonButton").style.backgroundColor = "#d3d3d3";
-    document.getElementById("cppButton").style.backgroundColor = "#b5e7a0";
-    document.getElementById("javaButton").style.backgroundColor = "#b5e7a0";
+    document.getElementById("cppButton").style.backgroundColor = "#ffffff";
+    document.getElementById("pythonButton").style.backgroundColor = "#ff6700" ; 
+    document.getElementById("javaButton").style.backgroundColor = "#ffffff";
+
+    
 }
 function cppClick(){
-    document.getElementById("javascriptButton").style.backgroundColor = "#b5e7a0";
-    document.getElementById("htmlButton").style.backgroundColor = "#b5e7a0";
-    document.getElementById("pythonButton").style.backgroundColor = "#b5e7a0";
-    document.getElementById("cppButton").style.backgroundColor = "#d3d3d3";
-    document.getElementById("javaButton").style.backgroundColor = "#b5e7a0";
+    document.getElementById("cppButton").style.backgroundColor = "#ff6700";
+    document.getElementById("pythonButton").style.backgroundColor = "#ffffff";
+    document.getElementById("javaButton").style.backgroundColor = "#ffffff";
+
+  
 }
 function javaClick(){
-    document.getElementById("javascriptButton").style.backgroundColor = "#b5e7a0";
-    document.getElementById("htmlButton").style.backgroundColor = "#b5e7a0";
-    document.getElementById("pythonButton").style.backgroundColor = "#b5e7a0";
-    document.getElementById("cppButton").style.backgroundColor = "#b5e7a0";
-    document.getElementById("javaButton").style.backgroundColor = "#d3d3d3";
+    document.getElementById("cppButton").style.backgroundColor = "#ffffff";
+    document.getElementById("pythonButton").style.backgroundColor = "#ffffff";
+    document.getElementById("javaButton").style.backgroundColor = "#ff6700";
+    
+   
 }
 resetCodeBtn.addEventListener('click', () => {
     // Clear ace editor
---
+++ test-extension/content.js
@@ -0,0 +1,19 @@
+window.onload=function(){
+    document.getElementById("submitButton").addEventListener("click", myFunc);
+}
+
+
+function myFunc()
+{
+    document.getElementById("inputText").value = "BYE";
+    // var languages = ["C++", "Java", "Python", "C"];
+	// var code = document.getElementById("inputText").value;
+	// console.log(code);
+	// var val = 0;
+	// for( i=0 ; i<code.length; i++)
+	// {
+	// 	console.log(val);
+	// 	val = val + code.charCodeAt(i);
+	// }
+	// document.getElementById("outputText").value = val + " is the answer\n bruh \n";
+}
