b7de10cda4ed34a966eb73935e9de2b60fdf7cd1
silentc1c4d4 <silentcicada3301@gmail.com>
2021-01-13 06:30:21 +0530

    removed bashScripts

--- cpp/cppDocker.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-
-docker run --volume=/Users/madhur/Desktop/projects/syntaxError/cpp:/usr/src/app meet /bin/bash -c "cd /usr/src/app && g++ -std=c++14 -o binary code.cpp && cat input.txt | ./binary > output.txt"
--- java/Main.java
+++ java/Main.java
@@ -1,5 +1,1 @@
-class Main {
-    public static void main( String []args ) {
-        System.out.println( "Hello World!" );
-    }
-}
+asdasd
