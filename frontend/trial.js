845ca4053399bda3df2e929fe0588bb4346b04a9
Yatin Khanna <yatinkhanna2000@gmail.com>
2021-05-08 06:58:56 +0530

    handled unreadable characters

--- frontend/trial.js
+++ frontend/trial.js
@@ -1,11 +1,24 @@
 document.addEventListener("keydown", (e) => {
     if ((e.ctrlKey || e.metaKey) && e.keyCode == 66) {
+        // selected text
         var selectedText = '';
         if (window.getSelection) {
             selectedText = window.getSelection().toString();
+            selectedText = filterText(selectedText);
         }
         alert("Your code has been copied! Run through the extension and put input if necessary!!");
 
         chrome.runtime.sendMessage({ greeting: selectedText });
     }
 });
+
+function filterText(unfilteredString) {
+    var filteredString = '';
+    for(i = 0; i<unfilteredString.length; i++){
+        if(unfilteredString.charCodeAt(i)>=32 && unfilteredString.charCodeAt(i)<=127) 
+            filteredString += unfilteredString[i];
+        else if(unfilteredString.charCodeAt(i)>=8 && unfilteredString.charCodeAt(i)<=13)
+            filteredString += unfilteredString[i];
+    }
+    return filteredString;
+}
