00b6f5bb397f9c77bf83bb83fee5b223f975b40a
CapriciousRebel <code.capriciousrebel@gmail.com>
2021-07-09 22:01:04 +0530

    add dotenv config

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const cors = require('cors')
 const fs = require('fs')
-const dotenv = require('dotenv')
+const dotenv = require('dotenv').config()
 const myfunc = require('./execCode.js')
 const app = express();
 app.use(cors())
